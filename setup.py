1e3ec0f1e5ab12d9ca66a09c508131a911a94feb
David Hagan <dhagan@mit.edu>
2015-06-03 14:20:32 -0400

    library commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+__version__ = '0.0.1'
+
+from distutils.core import setup
+
+setup(
+    name = 'py-opc',
+    packages = [],
+    version = __version__,
+    description = 'Python libary for operating the Alphasense OPC-N2',
+    author = 'David H Hagan',
+    author_email = 'david@davidhhagan.com',
+    license = 'MIT',
+    url = 'https://github.com/dhhagan/py-opc',
+    install_requires = ['spidev'],
+    keywords = ['opc', 'alphasense', 'atmospheric chemistry'],
+    classifiers = [
+        'Development Status :: 1 - alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: System :: Hardware',
+        'Topic :: System :: Hardware Drivers',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

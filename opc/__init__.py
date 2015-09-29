6bc6e44e499874d8fc2148e8421ca54df8abb295
David Hagan <dhagan@mit.edu>
2015-09-29 15:04:42 -0400

    added support for OPC-N1

--- opc/__init__.py
+++ opc/__init__.py
@@ -9,6 +9,202 @@ import warnings
 __all__ = ['OPCN2']
 __version__ = get_distribution('opc').version
 
+class OPC(object):
+    '''
+    Generic class for any Alphasense OPC Instance
+    '''
+    def __init__(self, spi_connection, debug = False, model = 'N2'):
+        self.cnxn       = spi_connection
+        self.debug      = debug
+        self.firmware   = None
+        self.model      = model
+
+        # Check to make sure the connection is a valid SpiDev instance
+        if not isinstance(spi_connection, spidev.SpiDev):
+            raise SPIError("This is not an instance of SpiDev.")
+
+        def __combine_bytes(self, LSB, MSB):
+            ''' Returns the combined Bytes '''
+            return (MSB << 8) | LSB
+
+        def _calculate_float(seld, byte_array):
+            ''' Returns a float from an array of 4 bytes '''
+            if len(byte_array) != 4:
+                return None
+
+            return struct.unpack('f', struct.pack('4B', *byte_array))[0]
+
+        def _calculate_mtof(self, mtof):
+            '''
+                calculates the average amount of time that particles in
+                this bin took to cross the path of the OPC units -> [micro-seconds]
+            '''
+            return mtof / 3.0
+
+        def _calculate_temp(self, vals):
+            ''' calculates the temperature in degrees celcius '''
+            if len(vals) < 4:
+                return None
+
+            return ((vals[3] << 24) | (vals[2] << 16) | (vals[1] << 8) | vals[0]) / 10.0
+
+        def _calculate_pressure(self, vals):
+            ''' calculate the pressure in pascals '''
+            if len(vals) < 4:
+                return None
+
+            return ((vals[3] << 24) | (vals[2] << 16) | (vals[1] << 8) | vals[0])
+
+        def _calculate_period(self, vals):
+            ''' calculate the sampling period in seconds '''
+            if len(vals) < 4:
+                return None
+
+            if self.firmware < 16:
+                return ((vals[3] << 24) | (vals[2] << 16) | (vals[1] << 8) | vals[0]) / 12e6
+            else:
+                return self._calculate_float(vals)
+
+        def _calculate_bin_boundary(self, val):
+            '''
+                Returns the bin boundary value in micrometers.
+                Assumes a 12-bit ADC with 17.5 um Full-Scale.
+            '''
+            fullscale   = 17.5    # micrometers
+            adc         = 12
+
+            return (val / (2**adc - 1)) * fullscale
+
+        def __repr__(self):
+            return "Alphasense OPC-{}".format(self.model)
+
+class OPCN1(OPC):
+    def __init__(self, spi_connection, debug = False, **kwargs):
+        self.spi_cnxn   = spi_connection
+        self.debug      = debug
+        self.model      = 'N1'
+
+        super(OPC, self).__init__(spi_connection = self.spi_cnxn,
+                                       debug = self.debug,
+                                       model = self.model)
+
+    def on(self):
+        ''' Turn on the OPC: returns a boolean value '''
+        b1 = self.cnxn.xfer([0x0C])[0]          # send the command byte
+
+        return True if b1 == 0xF3 else False
+
+    def off(self):
+        ''' Turn off the OPC: returns a boolean value '''
+        b1 = self.cnxn.xfer([0x03])[0]          # send the command byte
+
+        return True if b1 == 0xF3 else False
+
+    def ping(self):
+        ''' returns the status of the OPC-N1 as a boolean '''
+        b = self.cnxn.xfer([0xCF])[0]           # send the command byte
+
+        return True if b == 0xF3 else False
+
+    def read_info_string(self):
+        ''' returns the firmware information for the OPC as a string '''
+
+        infostring = []
+        command = 0x3F
+
+        # Send the command byte and sleep for 9 ms
+        self.cnxn.xfer([command])
+        sleep(9e-3)
+
+        # Read the info string by sending 60 empty bytes
+        for i in range(60):
+            resp = self.cnxn.xfer([0x00])[0]
+            infostring.append(chr(resp))
+
+        # Set the Firmware variable
+        try:
+            self.firmware = int(''.join(infostring[25:28]))
+        except:
+            raise FirmwareError("Cannot determine correct firmware for this OPC.")
+
+        return ''.join(infostring)
+
+    def read_histogram(self):
+        '''
+        Reads and resets the histogram bins, returning the data as a dictionary.
+        If the data transfer fails, returns None
+        '''
+        resp = []
+        data = {}
+
+        # command byte
+        command = 0x30
+
+        # Send the command byte
+        self.cnxn.xfer([command])
+
+        # Wait 10 ms
+        sleep(10e-3)
+
+        # read the histogram
+        for i in range(62):
+            r = self.cnxn.xfer([0x00])[0]
+            resp.append(r)
+
+        # convert to real things and store in dictionary!
+        data['Bin 0']           = self._combine_bytes(resp[0], resp[1])
+        data['Bin 1']           = self._combine_bytes(resp[2], resp[3])
+        data['Bin 2']           = self._combine_bytes(resp[4], resp[5])
+        data['Bin 3']           = self._combine_bytes(resp[6], resp[7])
+        data['Bin 4']           = self._combine_bytes(resp[8], resp[9])
+        data['Bin 5']           = self._combine_bytes(resp[10], resp[11])
+        data['Bin 6']           = self._combine_bytes(resp[12], resp[13])
+        data['Bin 7']           = self._combine_bytes(resp[14], resp[15])
+        data['Bin 8']           = self._combine_bytes(resp[16], resp[17])
+        data['Bin 9']           = self._combine_bytes(resp[18], resp[19])
+        data['Bin 10']          = self._combine_bytes(resp[20], resp[21])
+        data['Bin 11']          = self._combine_bytes(resp[22], resp[23])
+        data['Bin 12']          = self._combine_bytes(resp[24], resp[25])
+        data['Bin 13']          = self._combine_bytes(resp[26], resp[27])
+        data['Bin 14']          = self._combine_bytes(resp[28], resp[29])
+        data['Bin 15']          = self._combine_bytes(resp[30], resp[31])
+        data['Bin1 MToF']       = self._calculate_mtof(resp[32])
+        data['Bin3 MToF']       = self._calculate_mtof(resp[33])
+        data['Bin5 MToF']       = self._calculate_mtof(resp[34])
+        data['Bin7 MToF']       = self._calculate_mtof(resp[35])
+        data['Temperature']     = self._calcualte_temp(resp[36:40])
+        data['Pressure']        = self._calculate_pressure(resp[40:44])
+        data['Sampling Period'] = self._calculate_period(resp[44:48])
+        data['Checksum']        = self._combine_bytes(resp[48], resp[49])
+        data['Sampling Period'] = self._calculate_period(resp[44:48])
+        data['Checksum']        = self._combine_bytes(resp[48], resp[49])
+        data['PM1']             = self._calculate_float(resp[50:54])
+        data['PM2.5']           = self._calculate_float(resp[54:58])
+        data['PM10']            = self._calculate_float(resp[58:])
+
+        # Calculate the sum of the histogram bins
+        histogram_sum = data['Bin 0'] + data['Bin 1'] + data['Bin 2']   + \
+                data['Bin 3'] + data['Bin 4'] + data['Bin 5'] + data['Bin 6']   + \
+                data['Bin 7'] + data['Bin 8'] + data['Bin 9'] + data['Bin 10']  + \
+                data['Bin 11'] + data['Bin 12'] + data['Bin 13'] + data['Bin 14'] + \
+                data['Bin 15']
+
+        # If debug is True, print out the bytes!
+        if self.debug:
+            count = 0
+            print ("Debugging the Histogram")
+            for each in resp:
+                print ("\t{0}: {1}".format(count, each))
+                count += 1
+
+        # Check that checksum and the least significant bits of the sum of histogram bins
+        # are equivilant
+        if (histogram_sum & 0x0000FFFF) != data['Checksum']:
+            warnings.warn("Data transfer was incomplete.")
+            return None
+
+        return data
+
 class OPCN2:
     '''
         Class instance for the Alphasense Optical Particle Counter (OPC-N2)
